d621ebfbb8e8b434afe8eb7b73de89942b89dcc3
Yuri Sarmanho <yuri.sarmanho@hotmail.com>
2022-01-08 15:48:00 -0300

    selecionador de anos

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -4,20 +4,22 @@ import ExpenseDate from './ExpenseDate.js'
 import Card from '../UI/Card'
 
 const ExpenseItem = (props) => {
-    
+
     const [title, setTitle] = useState(props.title)
-    const changeTitle = () =>{
+    const changeTitle = () => {
         setTitle('updated!!')
     }
     return (
-        <Card className="expense-item">
-            <ExpenseDate date={props.date} />
-            <div className="expense-item__description">
-                <h2>{title}</h2>
-                <div className="expense-item__price">${props.amount}</div>
-            </div>
-            <button onClick={changeTitle}>Change Title</button>
-        </Card>
+        <div>
+            <Card className="expense-item">
+                <ExpenseDate date={props.date} />
+                <div className="expense-item__description">
+                    <h2>{title}</h2>
+                    <div className="expense-item__price">${props.amount}</div>
+                </div>
+                <button onClick={changeTitle}>Change Title</button>
+            </Card>
+        </div>
     )
 }
 
