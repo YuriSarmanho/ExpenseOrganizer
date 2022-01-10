eb388214b2cb2c3657cc458126b26d1d2644aca0
Yuri Sarmanho <yuri.sarmanho@hotmail.com>
2022-01-10 14:02:28 -0300

    add expenses

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,7 +1,8 @@
+import React, { useState } from 'react'
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpenses/NewExpense"
 const App = () => {
-  const expenses = [
+  const DUMMY_EXPENSE = [
     {
       id: 'e1',
       title: 'Toilet Paper',
@@ -22,9 +23,12 @@ const App = () => {
       date: new Date(2021, 5, 12),
     },
   ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSE)
+
   const addExpenseHandler = (expense) => {
-    console.log('In App.js')
-    console.log(expense)
+    setExpenses((prevExpenses)=>{
+      return [expense,...prevExpenses]
+    })
   }
 
   return (
--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,23 +1,17 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate.js'
 import Card from '../UI/Card'
 
 const ExpenseItem = (props) => {
-
-    const [title, setTitle] = useState(props.title)
-    const changeTitle = () => {
-        setTitle('updated!!')
-    }
     return (
         <div>
             <Card className="expense-item">
                 <ExpenseDate date={props.date} />
                 <div className="expense-item__description">
-                    <h2>{title}</h2>
+                    <h2>{props.title}</h2>
                     <div className="expense-item__price">${props.amount}</div>
                 </div>
-                <button onClick={changeTitle}>Change Title</button>
             </Card>
         </div>
     )
