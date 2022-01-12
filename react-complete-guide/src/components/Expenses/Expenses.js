9469ab643e974087f81f7e223306c2e7a391c0d9
Yuri Sarmanho <yuri.sarmanho@hotmail.com>
2022-01-12 14:21:26 -0300

    projeto finalizado

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,43 +1,34 @@
-import React, { useState } from 'react'
-import ExpenseItem from './ExpenseItem'
-import ExpensesFilter from './ExpensesFilter.js'
-import Card from '../UI/Card'
-import './Expenses.css'
+import React, { useState } from 'react';
+
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
+import './Expenses.css';
 
 const Expenses = (props) => {
-    const [filteredYear, setFilteredYear] = useState('2020')
+  const [filteredYear, setFilteredYear] = useState('2020');
 
-    const filterChangeHandler = (selectedYear) => {
-        setFilteredYear(selectedYear)
-    }
-    const filteredExpenses = props.items.filter(expenses => {
-        return expenses.date.getFullYear().toString() === filteredYear
-    })
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
 
-    let expensesContent = <p>No expenses found.</p>
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
-    if (filteredExpenses.length > 0) {
-        expensesContent = filteredExpenses.map((expenses) => (
-            <ExpenseItem
-                key={expenses.id}
-                title={expenses.title}
-                amount={expenses.amount}
-                date={expenses.date}
-            />
-        ))
-    }
+  return (
+    <div>
+      <Card className='expenses'>
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </div>
+  );
+};
 
-
-    return (
-        <div>
-            <Card className="expenses">
-                <ExpensesFilter
-                    selected={filteredYear}
-                    onChangeFilter={filterChangeHandler}
-                />
-                {expensesContent}
-            </Card>
-        </div>
-    )
-}
-export default Expenses
+export default Expenses;
--- react-complete-guide/src/components/Expenses/ExpensesList.js
+++ react-complete-guide/src/components/Expenses/ExpensesList.js
@@ -1,22 +1,25 @@
-import React from 'react'
-import ExpenseItem from './ExpenseItem'
-import './ExpensesList.css'
-const ExpensesList = (props) => {
-    if (props.items.length === 0) {
-        return <h2 className='expenses-list__fallback'>No expenses found.</h2>
-    }
+import React from 'react';
 
-    return (
-        <ul className='expenses-list'>
-            {props.items.map((expenses) => (
-                <ExpenseItem
-                    key={expenses.id}
-                    title={expenses.title}
-                    amount={expenses.amount}
-                    date={expenses.date}
-                />
-            ))}
-        </ul>
-    )
-}
-export default ExpensesList
+import ExpenseItem from './ExpenseItem';
+import './ExpensesList.css';
+
+const ExpensesList = (props) => {
+  if (props.items.length === 0) {
+    return <h2 className='expenses-list__fallback'>Found no expenses.</h2>;
+  }
+
+  return (
+    <ul className='expenses-list'>
+      {props.items.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default ExpensesList;
